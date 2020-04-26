13a16b051f64a353f795147e7f27b22bd1f09e2c
AlexeySukhanov <webdevforbiz@gmail.com>
2020-04-26 07:36:13 +0300

    ADD: mu-plugins activator

---
+++ wp-content/mu-plugins/mu-plugins-activator.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ * Uploads all must-use plugins
+ */
+require WPMU_PLUGIN_DIR . '/radio-buttons-for-taxonomies/radio-buttons-for-taxonomies.php';
+require WPMU_PLUGIN_DIR . '/advanced-custom-fields-pro/acf.php';
