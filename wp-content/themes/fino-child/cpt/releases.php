cffa5a684f36d14f6b3da321e7ecfc187c58c150
AlexeySukhanov <webdevforbiz@gmail.com>
2020-04-26 04:02:17 +0300

    ADD: releases taxonomies

--- wp-content/themes/fino-child/cpt/releases.php
+++ wp-content/themes/fino-child/cpt/releases.php
@@ -66,3 +66,177 @@ function filters_releases_messages( $messages ) {
 
 }
 add_filter( 'post_updated_messages', 'filters_releases_messages' );
+
+
+/*
+ * Adds Releases CPT taxonomies
+ */
+function add_formats_taxonomies() {
+
+    register_taxonomy('genre',
+        array('releases'),
+        array(
+            'hierarchical' => false,
+            'labels' => array(
+                'name' => 'Genres',
+                'singular_name' => 'Genre',
+                'search_items' =>  'Search Genres',
+                'popular_items' => 'Popular Genres',
+                'all_items' => 'All Genres',
+                'parent_item' => null,
+                'parent_item_colon' => null,
+                'edit_item' => 'Edit Genre',
+                'update_item' => 'Update Genre',
+                'add_new_item' => 'Add New Genre',
+                'new_item_name' => 'New Genre Name',
+                'separate_items_with_commas' => 'Separate genres with commas',
+                'add_or_remove_items' => 'Add or remove genres',
+                'choose_from_most_used' => 'Choose from mose used genres',
+                'menu_name' => 'Genres'
+            ),
+            'public' => true,
+            'show_in_nav_menus' => true,
+            'show_ui' => true,
+            'show_tagcloud' => true,
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'genre',
+                'hierarchical' => false
+            ),
+        )
+    );
+
+    register_taxonomy('style',
+        array('releases'),
+        array(
+            'hierarchical' => false,
+            'labels' => array(
+                'name' => 'Styles',
+                'singular_name' => 'Style',
+                'search_items' =>  'Search Styles',
+                'popular_items' => 'Popular Styles',
+                'all_items' => 'All Styles',
+                'parent_item' => null,
+                'parent_item_colon' => null,
+                'edit_item' => 'Edit Style',
+                'update_item' => 'Update Style',
+                'add_new_item' => 'Add New Style',
+                'new_item_name' => 'New Style Name',
+                'separate_items_with_commas' => 'Separate styles with commas',
+                'add_or_remove_items' => 'Add or remove styles',
+                'choose_from_most_used' => 'Choose from mose used styles',
+                'menu_name' => 'Styles'
+            ),
+            'public' => true,
+            'show_in_nav_menus' => true,
+            'show_ui' => true,
+            'show_tagcloud' => true,
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'style',
+                'hierarchical' => false
+            ),
+        )
+    );
+
+    register_taxonomy('format',
+        array('releases'),
+        array(
+            'hierarchical' => false,
+            'labels' => array(
+                'name' => 'Formats',
+                'singular_name' => 'Format',
+                'search_items' =>  'Search Formats',
+                'popular_items' => 'Popular Formats',
+                'all_items' => 'All Formats',
+                'parent_item' => null,
+                'parent_item_colon' => null,
+                'edit_item' => 'Edit Format',
+                'update_item' => 'Update Format',
+                'add_new_item' => 'Add New Format',
+                'new_item_name' => 'New Format Name',
+                'separate_items_with_commas' => 'Separate formats with commas',
+                'add_or_remove_items' => 'Add or remove formats',
+                'choose_from_most_used' => 'Choose from mose used formats',
+                'menu_name' => 'Formats'
+            ),
+            'public' => true,
+            'show_in_nav_menus' => true,
+            'show_ui' => true,
+            'show_tagcloud' => true,
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'format',
+                'hierarchical' => false
+            ),
+        )
+    );
+
+    register_taxonomy('country',
+        array('releases'),
+        array(
+            'hierarchical' => false,
+            'labels' => array(
+                'name' => 'Countries',
+                'singular_name' => 'Country',
+                'search_items' =>  'Search Countries',
+                'popular_items' => 'Popular Countries',
+                'all_items' => 'All Countries',
+                'parent_item' => null,
+                'parent_item_colon' => null,
+                'edit_item' => 'Edit Country',
+                'update_item' => 'Update Country',
+                'add_new_item' => 'Add New Country',
+                'new_item_name' => 'New Country Name',
+                'separate_items_with_commas' => 'Separate countries with commas',
+                'add_or_remove_items' => 'Add or remove countries',
+                'choose_from_most_used' => 'Choose from mose used countries',
+                'menu_name' => 'Countries'
+            ),
+            'public' => true,
+            'show_in_nav_menus' => true,
+            'show_ui' => true,
+            'show_tagcloud' => true,
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'country',
+                'hierarchical' => false
+            ),
+        )
+    );
+
+    register_taxonomy('year',
+        array('releases'),
+        array(
+            'hierarchical' => false,
+            'labels' => array(
+                'name' => 'Years',
+                'singular_name' => 'Year',
+                'search_items' =>  'Search Years',
+                'popular_items' => 'Popular Years',
+                'all_items' => 'All Years',
+                'parent_item' => null,
+                'parent_item_colon' => null,
+                'edit_item' => 'Edit Year',
+                'update_item' => 'Update Year',
+                'add_new_item' => 'Add New Year',
+                'new_item_name' => 'New Year Value',
+                'separate_items_with_commas' => 'Separate years with commas',
+                'add_or_remove_items' => 'Add or remove years',
+                'choose_from_most_used' => 'Choose from mose used years',
+                'menu_name' => 'Years'
+            ),
+            'public' => true,
+            'show_in_nav_menus' => true,
+            'show_ui' => true,
+            'show_tagcloud' => true,
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'year',
+                'hierarchical' => false
+            ),
+        )
+    );
+
+}
+add_action( 'init', 'add_formats_taxonomies', 0 );
