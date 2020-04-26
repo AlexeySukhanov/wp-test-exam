cf078f48ba0e9de74b5358a0a69a775bc15b9ce8
AlexeySukhanov <webdevforbiz@gmail.com>
2020-04-26 04:03:00 +0300

    ADD: year select for year taxomony

---
+++ wp-content/themes/fino-child/assets/js/custom.js
@@ -0,0 +1,15 @@
+/*
+ * Custom JS code
+ *
+ */
+
+
+jQuery(document).ready(function($){
+
+    // Year select settings
+    $('#tag-name').yearselect({
+        start: 1860,
+        order:'desc'
+    });
+
+});
