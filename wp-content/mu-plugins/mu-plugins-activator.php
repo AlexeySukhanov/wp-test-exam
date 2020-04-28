e3f5ae74d2fabb794cdeaf03b529d6cb9fd87e0c
AlexeySukhanov <webdevforbiz@gmail.com>
2020-04-28 11:18:33 +0300

    ADD: facetwp plugin

--- wp-content/mu-plugins/mu-plugins-activator.php
+++ wp-content/mu-plugins/mu-plugins-activator.php
@@ -4,3 +4,4 @@
  */
 require WPMU_PLUGIN_DIR . '/radio-buttons-for-taxonomies/radio-buttons-for-taxonomies.php';
 require WPMU_PLUGIN_DIR . '/advanced-custom-fields-pro/acf.php';
+require WPMU_PLUGIN_DIR . '/facetwp/index.php';
